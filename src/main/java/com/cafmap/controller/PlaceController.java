e83d98778fa8e069c76f2bca1d91fbac977f0a3c
hjm8333 <35636560+hjm8333@users.noreply.github.com>
2023-08-07 18:02:09 +0900

    import 정리

--- src/main/java/com/cafmap/controller/PlaceController.java
+++ src/main/java/com/cafmap/controller/PlaceController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
-import java.io.File;
 import java.util.HashMap;
 import java.util.List;
 
